320366e99655e142a869a18d26d250a248447e80
Roothex200 <sabiturrahman60@gmail.com>
2024-07-13 20:27:50 +0600

    added main.dart

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:get/get.dart';
+import 'package:live_tv/presentation/auth/login/presentation/views/login_screen.dart';
+import 'package:live_tv/presentation/auth/otp_verify/presentation/views/opt_verify_screen.dart';
+import 'package:live_tv/presentation/auth/registration/presentation/views/registration_screen.dart';
+import 'package:live_tv/presentation/auth/reset_password/presentation/views/reset_password_screen.dart';
+import 'package:live_tv/presentation/details/presentation/view/details_page.dart';
+import 'package:live_tv/presentation/home/presentation/view/home_page.dart';
+import 'package:live_tv/presentation/profile/presentation/view/profile.dart';
+import 'package:live_tv/presentation/splash/splash_screen.dart';
+import 'package:live_tv/presentation/subscription/view/presentation/subscription_page.dart';
+import 'package:live_tv/utils/value/colors/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryAppBlueColor),
+        useMaterial3: true,
+      ),
+      home: const SubscriptionScreen(),
+    );
+  }
+}
